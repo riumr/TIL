bc49b7c5bdc81163e777b25f07fd5bea7caca5a8
riumr <atrium_grate.0j@icloud.com>
2022-08-23 16:58:09 +0900

    220823 추가

--- 문제모음/8월/220821/1476.py
+++ 문제모음/8월/220821/1476.py
@@ -1,9 +1,17 @@
 # 우리가 알고 있는 연도로 몇 년인지 구하는 프로그램을 작성하시오.
-# 브루트포스
+# 브루트포스 # 정수론
 import sys
 sys.stdin = open("1476.txt")
 input = sys.stdin.readline
 
 E, S, M = map(int, input().split())
-Y = E*15+S*28+M*19
-print(Y)
+# 15를 넘으면 E가 1이 된다.
+# 28을 넘으면 S가 1이 된다.
+# 19을 넘으면 M이 1이 된다.
+if Y > 15:
+    E = 1
+if Y > 28:
+    S = 1
+if Y > 19:
+    M = 1
+print(E, S, M)
