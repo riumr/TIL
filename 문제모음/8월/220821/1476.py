5f8fa6f2b2d2053de7bfe7b26c7dd8325d4863b4
riumr <atrium_grate.0j@icloud.com>
2022-08-24 17:19:22 +0900

    220824 수정

--- 문제모음/8월/220821/1476.py
+++ 문제모음/8월/220821/1476.py
@@ -8,10 +8,22 @@ E, S, M = map(int, input().split())
 # 15를 넘으면 E가 1이 된다.
 # 28을 넘으면 S가 1이 된다.
 # 19을 넘으면 M이 1이 된다.
-if Y > 15:
-    E = 1
-if Y > 28:
-    S = 1
-if Y > 19:
-    M = 1
-print(E, S, M)
+# 모든 수가 1씩 증가한다.
+e = 1
+s = 1
+m = 1
+Y = 1
+while True:
+    if e == E and s == S and m == M:
+        break
+    Y += 1
+    e += 1
+    s += 1
+    m += 1
+    if e > 15:
+        e = 1
+    if s > 28:
+        s = 1
+    if m > 19:
+        m = 1
+print(Y)
