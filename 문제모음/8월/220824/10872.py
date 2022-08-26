f4c7cfcb6cbc3b9fa09377d2e105d7bf5eb85cf9
riumr <atrium_grate.0j@icloud.com>
2022-08-26 14:59:10 +0900

    220826 추가

--- 문제모음/8월/220824/10872.py
+++ 문제모음/8월/220824/10872.py
@@ -1,1 +1,13 @@
 # 0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+# 구현 # 조합론 # 재귀
+
+import sys
+sys.stdin = open("10872.txt")
+input = sys.stdin.readline
+
+# N! = N*N-1...1
+N = int(input())
+n = 1
+for i in range(N, 1, -1):
+    n *= i
+print(n)
