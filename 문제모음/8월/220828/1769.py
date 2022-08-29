b65ede39468a5b99ed0a40e7bb39b158eb4f19f1
riumr <atrium_grate.0j@icloud.com>
2022-08-29 16:05:42 +0900

    220829 추가

---
+++ 문제모음/8월/220828/1769.py
@@ -0,0 +1,19 @@
+# 변환의 과정을 몇 번 거쳐야 Y가 한 자리 수가 되는지 구하라. Y가 3의 배수이면 YES를 출력한다.
+# 수학 # 구현 # 문자열 # 재귀
+
+# 변환과정 : 각 자리 수를 모두 더한다.
+import sys
+import math
+sys.stdin = open("1769.txt")
+input = sys.stdin.readline
+
+X = int(input())
+# 자리 수의 길이
+# 자리 수가 한 자리가 될 때까지 반복
+while len_X >= 1:
+    len_X = log(X)
+    for i in range(len_X, -1, -1):
+        Y += (X//10**i)
+        X %= 10**i
+    X = Y
+print(X, "\nYES") if Y % 3 == 0 else print(X, "\nNO")
