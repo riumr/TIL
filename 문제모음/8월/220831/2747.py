4324247390b1b8549992f82f211dcf12f8649b8b
riumr <atrium_grate.0j@icloud.com>
2022-09-01 17:34:49 +0900

    220901 추가

---
+++ 문제모음/8월/220831/2747.py
@@ -0,0 +1,16 @@
+# n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+# 수학 # 구현 # 재귀로 가능
+
+# 피보나치 수 : Fn = Fn-1 + Fn-2 (n ≥ 2)
+# c = a+b
+
+import sys
+sys.stdin = open("2747.txt")
+
+
+
+
+# def fibo(number):
+
+
+# if __name__ == '__main__':
