02a4f3f135aafa127b10dcf5b0afb115668b154e
riumr <atrium_grate.0j@icloud.com>
2022-09-07 17:55:46 +0900

    220907 추가

--- 문제모음/8월/220831/2747.py
+++ 문제모음/8월/220831/2747.py
@@ -4,48 +4,49 @@
 # 피보나치 수 : Fn = Fn-1 + Fn-2 (n ≥ 2)
 # 이전의 두 수를 더한다.
 # c = a+b
+# 1 1 2 3
 
 import sys
 sys.stdin = open("2747.txt")
 
-# 이전 첫번째 수는 1
-before01 = 1
-# 이전 두번째 수는
-before02 = 1
-# 단계는 0부터 시작
-step = 0
-n = int(input())
-# 단계가 n보다 작으면
-while step < n:
-    # result의 시작은 0
-    result = 0
-    # 이전의 수를 더한다
-    result = before01+before02
-    # 다음에 더할 수는 result이다
-    before01 = result
-    before02 = before01
-    step += 1
-print(result)
+# # n이 주어졌을 때
+# n = int(input())
+
+# f0 = 0
+# # 첫번째 수
+# f1 = 1
+# step = 1
+# # 두번째 수 result = f0+f1
+# # 다음으로 더할 수
+# # f0 = f1
+# # f1 = result
+# # 계산할 때마다 1에서 1씩 증가 : step = 1 미리 선언
+# # 반복할 계산
+# while step < n:
+#     step += 1
+#     result = f0+f1
+#     # 다음으로 더할 수
+#     f0 = f1
+#     f1 = result
+# if step == 1:
+#     result = 1
+# print(result)
 
 # 재귀로 구현
-# 반복할 argument는 before과 step이다.
 
 
-# def fibo(before, result, step):
-#     # 단계가 n보다 작으면
-#     if step < n:
-#         result += before
-#         before = result
-#         step += 1
-#         fibo(result, result, step)
-#     # 결과출력
-#     else:
-#         print(result, step)
+def fibo(f0, f1, step):
+    if step < n:
+        step += 1
+        result = f0 + f1
+        fibo(f1, result, step)
+    else:
+        print(step, f1)
+
+# a = 0
+# b = 1
+# s = 1
 
 
-# if __name__ == '__main__':
-#     b = 1
-#     r = 1
-#     s = 0
-#     n = int(input())
-#     fibo(b, r, s)
+n = int(input())
+fibo(0, 1, 1)
