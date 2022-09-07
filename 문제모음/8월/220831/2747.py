ae75958067886ea1eef09fe0f2d6b8e89993ca98
riumr <atrium_grate.0j@icloud.com>
2022-09-07 23:46:22 +0900

    220907 수정

--- 문제모음/8월/220831/2747.py
+++ 문제모음/8월/220831/2747.py
@@ -9,44 +9,44 @@
 import sys
 sys.stdin = open("2747.txt")
 
-# # n이 주어졌을 때
-# n = int(input())
+# n이 주어졌을 때
+n = int(input())
 
-# f0 = 0
-# # 첫번째 수
-# f1 = 1
-# step = 1
-# # 두번째 수 result = f0+f1
-# # 다음으로 더할 수
-# # f0 = f1
-# # f1 = result
-# # 계산할 때마다 1에서 1씩 증가 : step = 1 미리 선언
-# # 반복할 계산
-# while step < n:
-#     step += 1
-#     result = f0+f1
-#     # 다음으로 더할 수
-#     f0 = f1
-#     f1 = result
-# if step == 1:
-#     result = 1
-# print(result)
+f0 = 0
+# 첫번째 수
+f1 = 1
+step = 1
+# 두번째 수 result = f0+f1
+# 다음으로 더할 수
+# f0 = f1
+# f1 = result
+# 계산할 때마다 1에서 1씩 증가 : step = 1 미리 선언
+# 반복할 계산
+while step < n:
+    step += 1
+    result = f0+f1
+    # 다음으로 더할 수
+    f0 = f1
+    f1 = result
+if step == 1:
+    result = 1
+print(result)
 
 # 재귀로 구현
 
 
-def fibo(f0, f1, step):
-    if step < n:
-        step += 1
-        result = f0 + f1
-        fibo(f1, result, step)
-    else:
-        print(step, f1)
+# def fibo(f0, f1, step):
+#     if step < n:
+#         step += 1
+#         result = f0 + f1
+#         fibo(f1, result, step)
+#     else:
+#         print(step, f1)  # 조건을 충족하지 않을 경우 이전의 결과값(f1)을 출력한다.
 
-# a = 0
-# b = 1
-# s = 1
+# # a = 0
+# # b = 1
+# # s = 1
 
 
-n = int(input())
-fibo(0, 1, 1)
+# n = int(input())
+# fibo(0, 1, 1)
