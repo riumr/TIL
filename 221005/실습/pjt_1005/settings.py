e611accd4c2d43f86077e7830cebcc4a062d103f
riumr <atrium_grate.0j@icloud.com>
2022-10-05 18:02:47 +0900

    221005 수정

--- 221005/실습/pjt_1005/settings.py
+++ 221005/실습/pjt_1005/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
-secret_key = open("articles/secret_key.txt", "r")
+secret_key = open("secret_key.txt", "r")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
