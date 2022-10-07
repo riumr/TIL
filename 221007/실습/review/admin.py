aedb99ba0aa8b001b2610b440f8fd93dbecdea38
riumr <atrium_grate.0j@icloud.com>
2022-10-07 16:41:14 +0900

    221007 수정

--- 221007/실습/review/admin.py
+++ 221007/실습/review/admin.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from .models import Review
 
 # Register your models here.
-
-
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ["Title", "content", "movie_name", "created_at"]
+    list_display = ["title", "content", "movie_name", "created_at"]
 
 
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
