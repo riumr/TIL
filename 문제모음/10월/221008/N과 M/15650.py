964094a56fee191ee569c04340455d12ac6de76e
riumr <atrium_grate.0j@icloud.com>
2022-10-16 23:46:40 +0900

    221016 수정

--- 문제모음/10월/221008/N과 M/15650.py
+++ 문제모음/10월/221008/N과 M/15650.py
@@ -1,6 +1,7 @@
 # 자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
 # 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
 # 고른 수열은 오름차순이어야 한다.
+# 백트래킹
 
 import sys
 
@@ -12,8 +13,8 @@ l = []
 # M개를 고른다.
 def backtracking():
     if len(l) == M and l == sorted(l):
-        print(l)
-    # 오름차순이다. ->
+        print(" ".join(map(str, l)))
+    # 오름차순이다. -> 오름차순인 경우를 출력한다.
     for i in range(1, N + 1):
         if i not in l:
             l.append(i)
