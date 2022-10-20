d2ae0eb6d2fccbbbf5bef110d8ed749f36b45f98
riumr <atrium_grate.0j@icloud.com>
2022-10-20 23:15:50 +0900

    221020 수정

--- 문제모음/10월/221016/N과 M(3)/15651.py
+++ 문제모음/10월/221016/N과 M(3)/15651.py
@@ -3,13 +3,25 @@
 # 1부터 N까지 자연수 중에서 M개를 고른 수열
 # 같은 수를 여러 번 골라도 된다.
 
+import sys
+
+sys.stdin = open("15651.txt")
+
 N, M = map(int, input().split())
+
+
+l = []
 
 
 def overlapped():
     if len(l) == M:
         print(l)
+        return  # 재귀오류가 발생하다가 return을 추가하니까 해결됐다.
+        # 같은 수를 여러번 골라도 된다.
     for i in range(1, N + 1):
         l.append(i)
         overlapped()
         l.pop()
+
+
+overlapped()
