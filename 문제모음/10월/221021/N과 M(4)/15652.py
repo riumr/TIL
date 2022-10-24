dd1bb327b3ed41447088afd6c49ffc8580253997
riumr <atrium_grate.0j@icloud.com>
2022-10-24 23:07:38 +0900

    221024 수정

--- 문제모음/10월/221021/N과 M(4)/15652.py
+++ 문제모음/10월/221021/N과 M(4)/15652.py
@@ -4,3 +4,17 @@
 # 같은 수를 여러 번 골라도 된다.
 # 고른 수열은 비내림차순이어야 한다.
 # 길이가 K인 수열 A가 A1 ≤ A2 ≤ ... ≤ AK-1 ≤ AK를 만족하면, 비내림차순이라고 한다.
+
+N, M = map(int, input().split())
+
+
+def dfs():
+    if len(l) == M and l == sorted(l, reverse=True):
+        print("".join(map(int, l)))
+    for i in range(1, N + 1):
+        l.append(i)
+        dfs()
+        l.pop()
+
+
+dfs()
